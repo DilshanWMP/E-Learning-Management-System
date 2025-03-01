82d4bd451d34d5ac4479253fcca4bfb92883be9e
DilshanWMP <praveenwmd@gmail.com>
2025-03-01 16:06:49 +0530

    Create a register section

--- project/src/Components/LoginRegister/LoginRegister.jsx
+++ project/src/Components/LoginRegister/LoginRegister.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './LoginRegister.css'
-import { FaUser, FaLock } from "react-icons/fa";
+import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
 
 function LoginRegister() {
   return (
@@ -18,14 +18,42 @@ function LoginRegister() {
                 </div>
 
                 <div className="remember-forget">
-                    <label><input type="checkbox" /
-                    >Remember me</label>
+                    <label><input type="checkbox" />Remember me</label>
                     <a href="#">Forget password?</a>
                 </div>
 
                 <button type="submit">Login</button>
                 <div className="register-link">
                     <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+            </form>
+        </div>
+
+        <div className="form-box register">
+            <form action=''>
+                <h1>Registration</h1>
+                <div className="input-box">
+                    <input type="text" placeholder='Username' required/>
+                    <FaUser className='icon' />
+                </div>
+
+                <div className="input-box">
+                    <input type="email" placeholder='Email' required/>
+                    <FaEnvelope className='icon' />
+                </div>
+
+                <div className="input-box">
+                    <input type="password" placeholder='Password' required/>
+                    <FaLock className='icon' />
+                </div>
+
+                <div className="remember-forget">
+                    <label><input type="checkbox" />I agree to the terms & conditions</label>
+                </div>
+
+                <button type="submit">Register</button>
+                <div className="register-link">
+                    <p>Already have an account? <a href="#">Login</a></p>
                 </div>
             </form>
         </div>
